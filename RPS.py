8aa7d53d894ab0d5420ca8d68d62713d7519882d
Nguyễn Minh Tuấn <tuannm1610@gmail.com>
2024-06-22 20:20:48 +0700

    Update code

--- RPS.py
+++ RPS.py
@@ -1,12 +1,19 @@
+import random
+
 moves = ["rock", "paper", "scissors"]
 
 
 class Player:
     def move(self):
-        return "rock"
+        pass
 
     def learn(self, my_move, their_move):
         pass
+
+
+class RandomPlayer(Player):
+    def move(self):
+        return random.choice(moves)
 
 
 def beats(one, two):
@@ -22,21 +29,43 @@ class Game:
         self.p1 = p1
         self.p2 = p2
 
-    def play_round(self):
+    def play_round(self, point1, point2):
         move1 = self.p1.move()
         move2 = self.p2.move()
+
         print(f"Player 1: {move1}  Player 2: {move2}")
+
+        if move1 == move2:
+            pass
+        elif beats(move1, move2):
+            point1 += 1
+            return point1
+        else:
+            point2 += 1
+            return point2
+
         self.p1.learn(move1, move2)
         self.p2.learn(move2, move1)
 
-    def play_game(self):
+        return point1, point2
+
+    def play_game(self, point1, point2):
         print("Game start!")
+
         for round in range(3):
             print(f"Round {round}:")
-            self.play_round()
+
+            point1, point2 = self.play_round(point1, point2)
+
+            print(f"Player 1: {point1} points  Player 2: {point2} points")
+
         print("Game over!")
 
 
 if __name__ == "__main__":
-    game = Game(Player(), Player())
-    game.play_game()
+    game = Game(RandomPlayer(), RandomPlayer())
+    point1 = 0
+    point2 = 0
+    game.play_game(point1, point2)
+    # test = RandomPlayer().move()
+    # print(test)
